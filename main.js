1f5890eab516351b094a4a7c9c9973489daaa7a0
14092006vanessa <117873659+14092006vanessa@users.noreply.github.com>
2023-11-07 21:57:57 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -27,6 +27,8 @@ document.querySelector('.tecla_tic').onclick = tocaSomtic;
 function tocaSomtom(){document.querySelector('#som_tecla_tom').play();}
 document.querySelector('.tecla_tom').onclick = tocaSomtom;
 
+const listadeTecla=document.querySelectorAll('.tecla');
+
 
 
 
